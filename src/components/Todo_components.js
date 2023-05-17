29fa4a854b5289f1488e54f7069f147908c6cc9d
loic <loic.tossou@epitech.eu>
2023-05-17 15:33:43 +0100

    Change value in button to add new tasks

--- src/components/Todo_components.js
+++ src/components/Todo_components.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react'
 
 function Todolist() {
-    const [fruits, setFruits] = useState([
-        "🍎 Appled",
-        "🍊 Orange",
-        "🍌 Banana",
-        "🍇 Grapes",
+    const [myTasks, setTaks] = useState([
+        "🍎 Create new components",
+        "🍊 Make pull request",
+        "🍌 Add some features",
+        "🍇 Delete get.js file",
       ])
     const [addTask, setAddTask] = useState('');
 
@@ -14,12 +14,12 @@ function Todolist() {
     };
 
     const deleteByValue = value => {
-        setFruits(oldValues => {
+        setTaks(oldValues => {
           return oldValues.filter(fruit => fruit !== value)
     })
     }
       const AddByValue = value => {
-        setFruits(current => [...current, value]);
+        setTaks(current => [...current, value]);
         setAddTask("")
       }
 
@@ -30,14 +30,14 @@ function Todolist() {
             <h1 className='text-center text-[#857E61] text-2xl font-bold py-4'>My To Do App</h1>
             <div className='flex flex-row justify-center items-center space-x-5'>
                 <input onChange={handleChange} value={addTask} type="text" placeholder='Saisissez la tâche à ajouter' className='border w-full p-2 py-sm-3 rounded-xl outline-none placeholder:font-extrabold' required/>
-                <button onClick={() => AddByValue(addTask)} className="bg-[#857E61] font-bold whitespace-nowrap px-4 py-2 rounded-xl text-white text-center hover:scale-125 transition duration-500 hover:duration-500">Ajouter</button>
+                <button onClick={() => AddByValue(addTask)} className="bg-[#857E61] font-bold whitespace-nowrap px-4 py-2 rounded-xl text-white text-center hover:scale-125 transition duration-500 hover:duration-500">Add</button>
             </div>
             <ul className='space-y-3'>
-                {fruits.map(fruit => {
+                {myTasks.map(myTasks => {
                     return (
-                    <li key={fruit} className='flex justify-between bg-white px-3 py-2 drop-shadow-lm rounded-lg'>
-                        <span>{fruit}</span>
-                        <button onClick={() => deleteByValue(fruit)} className='text-[#857E61] font-extrabold'>Delete</button>
+                    <li key={myTasks} className='flex justify-between bg-white px-3 py-2 drop-shadow-lm rounded-lg'>
+                        <span>{myTasks}</span>
+                        <button onClick={() => deleteByValue(myTasks)} className='text-[#857E61] font-extrabold'>Delete</button>
                     </li>
                 )
                 })}
